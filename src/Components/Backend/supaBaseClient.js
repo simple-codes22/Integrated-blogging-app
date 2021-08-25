dcd2c89df4d231ae792977b6056f7364390dc37c
AKANBI <simeonakanbi99@gmail.com>
2021-08-25 16:26:58 +0100

    Made my app safer (sort of)

--- src/Components/Backend/supaBaseClient.js
+++ src/Components/Backend/supaBaseClient.js
@@ -1,7 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
+require('dotenv').config();
 
-const supabaseUrl = 'https://lwjgteyoakzezinsxgxr.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyODUxOTY5OCwiZXhwIjoxOTQ0MDk1Njk4fQ.MnYxO_bSLC9GzCVDSf9xaNFyukinvGj_IVJDozqcFlg'
+
+
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_ANON_KEY
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
