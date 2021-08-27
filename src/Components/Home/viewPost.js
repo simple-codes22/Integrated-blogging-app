3b4291eb32dc94ab253ae6646fb53219a288e9e6
AKANBI <simeonakanbi99@gmail.com>
2021-08-27 16:41:46 +0100

    Articles are now viewable in a new page

--- src/Components/Home/viewPost.js
+++ src/Components/Home/viewPost.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import { Box, makeStyles } from '@material-ui/core';
+import { Box, makeStyles, Typography } from '@material-ui/core';
 import supabase from "../Backend/supaBaseClient";
 
 
@@ -17,19 +17,54 @@ const ViewPost = (props) => {
     let { id } = useParams();
 
     const [currentArticle, newArticle] = useState([]);
+    
+    useEffect(() => {
+        getArticle();
+    }, [props.id])
+    
+    useEffect(() => {
+        if (currentArticle.length !== 0) {
+            document.title = currentArticle[0].title;
+        } else {
+            document.title = 'Blogging App | Article';
+        }
+    }, [currentArticle]);
+    
     const getArticle = async () => { // Gets the article clicked on
-        let data = await supabase.from('posts').select("*").eq('id', id);
+        const data = await supabase.from('posts').select("*, blogger_profiles(username)").eq('id', id);
         return newArticle(data.data);
     }
+    
+    const CheckLoad = () => {
+        if (currentArticle.length !== 0) {
+            console.log('Article Loaded');
+            return (
+                <Box container component='article'>
+                    <Box container component='div'>
+                        <Box component='div'><Typography variant='h3'>{currentArticle[0].title}</Typography></Box>
+                        <Box component='div'><Typography variant='subtitle1'> By {currentArticle[0].blogger_profiles.username}</Typography></Box>
+                        <Box component='div'><Typography variant='caption'>Posted at {currentArticle[0].inserted_at}</Typography></Box>
+                    </Box>
+                    <Box container component='div'>
+                        {currentArticle[0].post_content}
+                    </Box>
+                </Box>
+            )
+        } else {
+            console.log('Article not Loaded');
+            return (
+                <>
+                </>
+            )
+        }
+    }
 
-    useEffect(() => {
-        console.log(id);
-        getArticle();
-        console.log(currentArticle);
-    }, [props.id])
+    console.log(currentArticle);
     return (
-        <Box component='div' className={useStyle.root}>
-
+        <Box component='section' className={useStyle.root}>
+            <Box component='div'>
+                <CheckLoad />
+            </Box>
         </Box>
     )
 }
